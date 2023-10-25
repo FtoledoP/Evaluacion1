05614de8867ea7cdc51f7ca5af29473ffaccd28d
Frnaco <fr.toledop@duocuc.cl>
2023-10-25 09:12:23 -0300

    Revertir commit, no firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  firebase: {
-    projectId: 'movile-fe3db',
-    appId: '1:1003133711063:web:37280f9eb3b96d6c237692',
-    storageBucket: 'movile-fe3db.appspot.com',
-    apiKey: 'AIzaSyA3yt0bNzhkvAG1-wrwv73yVgT22AceNT0',
-    authDomain: 'movile-fe3db.firebaseapp.com',
-    messagingSenderId: '1003133711063',
-    measurementId: 'G-K72P8NWD59',
-  },
   production: true
 };
