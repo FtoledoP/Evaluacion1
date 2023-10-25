2fd4971382afd87353466a42ba1aad11b6d06b63
Frnaco <fr.toledop@duocuc.cl>
2023-10-25 09:43:25 -0300

    esta vez si pudimos implementar firebase correctamente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'movile-71c1f',
+    appId: '1:929675018378:web:9058c8562cad73fcb66d73',
+    storageBucket: 'movile-71c1f.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyC_eO6iqnY-l8rJjUTofBdAorsZhJl2aOg',
+    authDomain: 'movile-71c1f.firebaseapp.com',
+    messagingSenderId: '929675018378',
+    measurementId: 'G-P7NTCKQSYW',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'movile-71c1f',
+    appId: '1:929675018378:web:9058c8562cad73fcb66d73',
+    storageBucket: 'movile-71c1f.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyC_eO6iqnY-l8rJjUTofBdAorsZhJl2aOg',
+    authDomain: 'movile-71c1f.firebaseapp.com',
+    messagingSenderId: '929675018378',
+    measurementId: 'G-P7NTCKQSYW',
+  },
   production: false
 };
 
