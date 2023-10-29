18f1f0291c5200ebd06a81059e7b88e4f02f1597
Frnaco <fab.betancourt@duocuc.cl>
2023-10-28 22:21:04 -0300

    Evaluacion 2 finalizada

--- src/app/restablecer-contra/restablecer-contra.page.ts
+++ src/app/restablecer-contra/restablecer-contra.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-restablecer-contra',
@@ -7,30 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestablecerContraPage implements OnInit {
 
-  usuario!:string;
-  password!:string
-  usuariosStorage!:any;
+  recForm: FormGroup;
+  password:any;
 
-  
-
-  constructor() { }
+  constructor(private fb: FormBuilder,
+              private userService: UserService) {
+    this.recForm = this.fb.group({
+      correo: ['', [Validators.required, Validators.email]],
+    })
+  }
 
   ngOnInit() {
   }
 
   resuperarContra(){
-    this.cargaInfoUsuario();
-    this.password = this.usuariosStorage[0].contraseña;
-    console.log(this.password);
-  }
-
-  cargaInfoUsuario() {
-    this.usuariosStorage = JSON.parse(localStorage.getItem('credenciales') || '{}');
-    console.log('Carga info usuario', this.usuariosStorage[0].nombre);
+    if(this.recForm.valid){
+      this.userService.getUser(this.recForm.value.correo).then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          console.log(doc.data());
+          const userTemporal = doc.data();
+          this.password = userTemporal['password'];
+        });
+      });
+    }
   }
 
   retroceder() {
     window.history.back();
   }
+
+  validationMessages = {
+    correo: {
+      required: 'Debe ingresar un correo.',
+      email: 'Debe ingresar un correo válido.'
+    }
+  };
 
 }
